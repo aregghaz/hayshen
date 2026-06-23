325abdb994f5d687b1a21a73e3b60393fdf0c348
TigranGhazaryan24 <tghazaryan@curavel.com>
2026-06-23 10:22:13 +0400

    fix warning

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
 
+const enableModernSassApi = (rules = []) => {
+  rules.forEach((rule) => {
+    if (Array.isArray(rule.oneOf)) {
+      enableModernSassApi(rule.oneOf);
+    }
+
+    if (!Array.isArray(rule.use)) {
+      return;
+    }
+
+    rule.use.forEach((loader) => {
+      if (
+        typeof loader === 'object' &&
+        loader.loader?.includes('sass-loader')
+      ) {
+        loader.options = {
+          ...loader.options,
+          api: 'modern',
+        };
+      }
+    });
+  });
+};
+
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+    configure: (webpackConfig) => {
+      enableModernSassApi(webpackConfig.module.rules);
+      return webpackConfig;
     },
   },
 };
